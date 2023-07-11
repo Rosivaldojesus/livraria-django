9d1c8c3f8b85b202819edda0d7ea639525f7098b
Rosivaldo de Jesus <js.rosivaldo@gmail.com>
2023-07-11 08:19:48 -0300

    Ajustes em itens compra

--- core/serializers.py
+++ core/serializers.py
@@ -1,33 +1,40 @@
-from rest_framework.serializers import ModelSerializer, CharField, SerializerMethodField
+from rest_framework.serializers import (ModelSerializer,
+                                        CharField,
+                                        SerializerMethodField)
 from rest_framework import serializers
 from core.models import Categoria, Editora, Autor, Livro, Compra, ItensCompra
 
 
 class CategoriaSerializer(ModelSerializer):
+
     class Meta:
         model = Categoria
         fields = "__all__"
 
 
 class EditoraSerializer(ModelSerializer):
+
     class Meta:
         model = Editora
         fields = "__all__"
 
 
 class EditoraNestedSerializer(ModelSerializer):
+
     class Meta:
         model = Editora
         fields = ("nome", )
 
 
 class AutorSerializer(ModelSerializer):
+
     class Meta:
         model = Autor
         fields = "__all__"
 
 
 class LivroSerializer(ModelSerializer):
+
     class Meta:
         model = Livro
         fields = "__all__"
@@ -73,16 +80,16 @@ class CompraSerializer(ModelSerializer):
         model = Compra
         # fields = "__all__"
         fields = ("id", "status", "usuario", "itens", "total")
-        
+
     def get_status(self, instance):
         return instance.get_status_display()
 
-  
+
 class CriarEditarItensSerializer(ModelSerializer):
     class Meta:
         model = ItensCompra
         fields = ('livro', 'quantidade')     
-    
+ 
 
 class CriarEditarCompraSerializer(ModelSerializer):
     itens = CriarEditarItensSerializer(many=True)
@@ -91,7 +98,7 @@ class CriarEditarCompraSerializer(ModelSerializer):
     class Meta:
         model = Compra
         fields = ('usuario', 'itens')
-        
+    
     def create(self, validated_data):
         itens = validated_data.pop('itens')
         compra = Compra.objects.create(**validated_data)
@@ -99,7 +106,7 @@ class CriarEditarCompraSerializer(ModelSerializer):
             ItensCompra.objects.create(compra=compra, **item)
         compra.save()
         return compra
-    
+
     def update(self, instance, validated_data):
         itens = validated_data.pop("itens")
         if itens:
--- core/views/compra.py
+++ core/views/compra.py
@@ -10,3 +10,11 @@ class CompraViewSet(ModelViewSet):
         if self.action == 'list' or self.action == 'retrieve':
             return CompraSerializer
         return CriarEditarCompraSerializer
+    
+    def get_queryset(self):
+        usuario = self.request.user
+        if usuario.groups.filter(name="Administradores"):
+            return Compra.objects.all()
+        
+        return Compra.objects.filter(usuario=usuario)
+    
